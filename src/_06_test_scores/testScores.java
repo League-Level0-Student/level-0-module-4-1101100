c628f83f86d495b4aba8cd4a3ced81a41ec1b6d9
League Student <league@iMac-17.attlocal.net>
2019-12-06 15:46:11 -0800

    Update testScores.java

--- src/_06_test_scores/testScores.java
+++ src/_06_test_scores/testScores.java
@@ -13,13 +13,13 @@ public class testScores {
 		
 		System.out.println(d);
 		
-		if (d >90) {
+		if (d >= 90) {
 			JOptionPane.showMessageDialog(null, "you did well");
 		}
-		else if (d < 90) {
+		else if (d < 90 && d >=80) {
 			JOptionPane.showMessageDialog(null, "you did ok");
 		}
-		else if (d < 80) { 
+		else if (d < 80 && d>=70) { 
 			JOptionPane.showMessageDialog(null, "you did poorly");
 		}
 		else if (d < 70) {
